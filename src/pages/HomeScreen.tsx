93514f5343535786e14076e43c05668a3bacf5d4
anker12 <tobias.as@hotmail.com>
2022-11-08 06:25:36 +0100

    supabase started fiddling

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,49 +1,44 @@
-import { IonContent, IonPage } from "@ionic/react";
+import { IonButton, IonContent, IonPage } from "@ionic/react";
 import TopBar from "../components/TopBar";
 import Card from "../components/Card";
+import signUp from "../utils/signUp";
 
-const HomeScreen: React.FC  = () => {
-    return (
-        <IonPage>
-            <TopBar title='Home' slot='fixed'/>
-            <IonContent>
-                <Card
-                    title="First card?" 
-                    content={
-                        'Gaming B)'
-                    }
-                />
-                <Card
-                    title="Second card?" 
-                    content={
-                        (<div>
-                            <span>Heyo</span>
-                        </div>)
-                    }
-                />
-                <Card
-                    title="Third card?" 
-                    content={
-                        'Gaming B)'
-                    }
-                />
-                <Card
-                    title="Fourth card?"
-                    subtitle={'Nested cards'} 
-                    content={(
-                        <>
-                            <Card 
-                                content={'Nested card 1'}
-                            />
-                            <Card 
-                                content={'Nested card 2'}
-                            />
-                        </>
-                    )}
-                />
-            </IonContent>
-        </IonPage>
-    );
-}
+const HomeScreen: React.FC  = () => (
+    <IonPage>
+        <TopBar title='Home' slot='fixed' />
+        <IonContent>
+            <Card
+                title="First card?"
+                content={'Gaming B)'} />
+            <Card
+                title="Second card?"
+                content={(
+                    <div>
+                        <IonButton
+                            onClick={() => signUp()}
+                        >
+                            Click me    
+                        </IonButton>
+                    </div>
+                )} 
+            />
+            <Card
+                title="Third card?"
+                content={'Gaming B)'} 
+            />
+            <Card
+                title="Fourth card?"
+                subtitle={'Nested cards'}
+                content={(
+                    <>
+                        <Card
+                            content={'Nested card 1'} />
+                        <Card
+                            content={'Nested card 2'} />
+                    </>
+                )} />
+        </IonContent>
+    </IonPage>
+)
 
 export default HomeScreen
---
+++ src/utils/signUp.ts
@@ -0,0 +1,26 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+const supabase = createClient(`${process.env.REACT_APP_API_URL}`, `${process.env.REACT_APP_API_KEY}`);
+
+const signUp = async () => {
+
+    const { error } = await supabase.auth.signUp({
+        email: 'example@email.com',
+        password: 'example-password',
+    });
+    console.log(error);
+    // console.log('User: ', user, 'session: ', session, error);
+    console.log(process.env.REACT_APP_SUPABASE_KEY);
+
+    if (!error) {
+        const { user, session, error } = await supabase.auth.signIn({
+            email: 'example@email.com',
+            password: 'example-password',
+        });
+        console.log(error);
+        console.log('user:', user, 'session: ', session);
+    }
+}
+
+export default signUp;
